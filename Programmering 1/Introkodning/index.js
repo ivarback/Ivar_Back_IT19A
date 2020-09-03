e2617d90d5d541136932dbec356957b5c399263b
Ivar_Back_IT19A <70194085+ivarback@users.noreply.github.com>
2020-09-03 09:58:26 +0200

    bra kobb

--- Programmering 1/Introkodning/index.js
+++ Programmering 1/Introkodning/index.js
@@ -18,5 +18,31 @@ console.log(`Hej ${name} du är ${age} år gammal`)
 
 //DOM - document objekt model
 
-let text = document.querySelector("text")
-text.html = `${name} är ${age} år gammal`
+let text = document.querySelector("#text")
+text.innerHTML = `${name} är ${age} år gammal`
+
+//skapar variabler för p-taggarna
+let mult = document.querySelector("#multiplikation")
+let div = document.querySelector("#division")
+let sub = document.querySelector("#subtraktion")
+let add = document.querySelector("#addition")
+
+mult.innerHTML = `multiplikation`
+div.innerHTML = `division`
+add.innerHTML = `addition`
+sub.innerHTML = `subtraktion`
+
+let button = document.querySelector("#knapp")
+// lägger till en eventlyssnare
+
+button.addEventListener("click", math)
+
+function math(event){
+console.log("math-funktionen körs")
+let num1 = parseFloat(document.querySelector("#number1").value)
+let num2 = parseFloat(document.querySelector("#number2").value)
+add.innerHTML = `${num1}+${num2}=${num1+num2}`
+div.innerHTML = `${num1}/${num2}=${num1/num2}`
+sub.innerHTML = `${num1}-${num2}=${num1-num2}`
+mult.innerHTML = `${num1}*${num2}=${num1*num2}`
+}
