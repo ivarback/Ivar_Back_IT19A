b917abe7e189469020a26c4e932c230228651193
Ivar_Back_IT19A <70194085+ivarback@users.noreply.github.com>
2020-09-08 09:41:21 +0200

    asd

---
+++ Programmering 1/Introkodning/variabler_IO/index.js
@@ -0,0 +1,20 @@
+
+
+let name="ivar"
+
+
+console.log (`Hej ${name}`)
+
+//1 skaa variablerna tal1 tal2 taö3
+//2 tilldela dessa variabler med värderna 345
+//3 räkna ut medelvärdet av dem
+
+let tal1=3
+let tal2=4
+let tal3=5
+
+medelvärde = (tal1+tal2+tal3)/3
+
+console.log(`${tal1}+${tal2}+${tal3}/3 =${medelvärde}`)
+
+let langd = prompt("ange värdet för rektangelns längd")
