c7c7c00edea13461b9df6e346b32c667695879b1
Ivar_Back_IT19A <70194085+ivarback@users.noreply.github.com>
2020-09-10 09:18:31 +0200

    hej

---
+++ Programmering 1/Inlämningsuppgift/inlamning1.js
@@ -0,0 +1,35 @@
+//Vad heter du ? Kokchun Giang
+//Hur gammal är du ?  29
+//Vad är din adress ? Kronhusgatan 9
+//Vad är ditt postnummer ? 411 05
+//Vilken stad bor du i ? Göteborg
+//ad är ditt telefonnummer ? 112
+
+//Gör följande utskrift i konsolen mha dina variabler. 
+
+//Information:
+//Namn:    Kokchun Giang
+//Ålder:     22 år
+//Adress:  Kronhusgatan 9 
+  //             411 05 Göteborg
+//Telefon: 112
+
+let namn = prompt ("Vad heter du?")
+
+let ålder = prompt ("Hur gammal är du?")
+
+let adress = prompt ("Var är din adress?")
+
+let postnummer = prompt ("Vad är ditt postnummer?")
+
+let stad = prompt ("Vilken stad bor du i?")
+
+let nummer = prompt ("Vad är ditt telefonnummer?")
+
+console.log (`Information`)
+
+console.log (`Namn: ${namn}`)
+console.log (`Ålder: ${ålder}`)
+console.log (`Adress: ${adress} ${postnummer}`)
+console.log (`Telefon: ${nummer} `)
+
