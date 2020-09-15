39ddac8cf3e3dc3dda178d66d338bfa0ac55a691
Ivar_Back_IT19A <70194085+ivarback@users.noreply.github.com>
2020-09-15 09:38:03 +0200

    positiv/negativ

---
+++ Programmering 1/if_satser/if_sats1.js
@@ -0,0 +1,14 @@
+
+
+console.log(`${4%2}`)
+// avgöra om taler är jämt eller inte
+
+//modulusoperator
+let tal=prompt ("Skriv ett tal")
+
+if (tal%2 === 1){
+    console.log("positiv")
+} else {
+    console.log("negativ")
+}
+
