37a5def44396866d50734419e1905e6b99979cb8
Ivar_Back_IT19A <70194085+ivarback@users.noreply.github.com>
2020-09-22 09:31:35 +0200

    hemuppgify

---
+++ Programmering 1/hemuppgift_if_satser/if_sats4.js
@@ -0,0 +1,14 @@
+let vikt = prompt ("Hur mycket väger ditt bagage?")
+
+let längd = prompt ("Hur många cm är längden på väskan?")
+
+let bredd = prompt ("Hur många cm är höjden på väskan?")
+
+let höjd = prompt ("Hur många cm är höjden på väskan")
+
+
+
+ if (längd > 56 && bredd > 41 && höjd >24 && vikt > 9 ){
+    console.log ("Du får åka med")
+} else {console.log ("Väskan är för stor eller väger för mycket")
+}
