b8d0743103867e0f9c206273f3747ed0bd14d3ca
Ivar_Back_IT19A <70194085+ivarback@users.noreply.github.com>
2020-10-01 09:02:25 +0200

    for laxa

--- Programmering 1/läxa_for_sats/forlaxa.js
+++ Programmering 1/läxa_for_sats/forlaxa.js
@@ -1,15 +1,20 @@
-let talseriejamna = document.querySelector("#talseriejamna")
-let talserieudda = document.querySelector ("#talserieudda")
-let lektioner = document.querySelector ("lektioner")
+let talseriejamna = document.querySelector("#talseriejamn")
+let talserieudda = document.querySelector("#talserieudda")
+let huy = document.querySelector("#lektioner")
 
+
+for (let i=0; i < 50; i++){
+    talseriejamna.innerHTML += `${2*i}, `
+    talserieudda.innerHTML += `${2*i+1}, `
+}
 
 
 
 //lektioner lista
-let lektioner = ["Svenska","Engelska","Programmering","Matematik","Idrott"]
-p_lektioner.innerHTML = "Mina lektioner är: <br/>"
+let lektioner = ["Svenska", "Engelska", "Programmering", "Matematik", "Idrott"]
+huy.innerHTML = "Mina lektioner är: <br/>"
 
-for (let k = 0; k < lektioner.length; k++){
-    p_lektioner.innerHTML += `${k+1}.${lektioner[k]} <br/>`
-    }
-    
+for (let k = 0; k < lektioner.length; k++) {
+    huy.innerHTML += `${k+1}.${lektioner[k]} <br/>`
+}
+
