c5dc4495f20212aba57f70913f2e3fb8b41d6681
Ivar_Back_IT19A <70194085+ivarback@users.noreply.github.com>
2020-10-13 14:03:48 +0200

    sdf

---
+++ Programmering 1/funktioner/funktioner1.js
@@ -0,0 +1,20 @@
+let namn = "Ivar"
+
+function sayHello(){
+    let ålder = 18
+    console.log(`Hej ${namn}`)
+}
+
+
+//anropar funktionen SayHello
+sayHello()
+
+function addera (tal1, tal2){
+    let summa = tal1+tal2
+    return summa
+
+}
+
+console.log (`2+5=${addera(2,5)}`)
+
+//skapa em sintraherafunktion
