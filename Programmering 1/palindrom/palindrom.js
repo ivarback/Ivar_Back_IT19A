4460c0487175d47effd00931b33154d4b640640a
Mariam656 <70264753+Mariam656@users.noreply.github.com>
2020-11-26 09:50:14 +0100

    jhg

--- Programmering 1/palindrom/palindrom.js
+++ Programmering 1/palindrom/palindrom.js
@@ -1,6 +1,35 @@
 
 //split() och reverse() och join()
-let palindrom = prompt("Skriv ett ord")
+
+let knapp = document.querySelector("#knapp")
+
+let p_rak = document.getElementById("rak")
+
+let p_reverse = document.getElementById("reverse")
+knapp.addEventListener("click",Palindrom)
+
+ function Palindrom() {
+    let string = ("Banan");
+    let res = string.split("");
+    res=res.join("")
+    console.log (res)
+   
+    
+    p_rak.innerHTML += res
+    
+    let rev = res.reverse("");
+    rev=rev.join("")
+    console.log (rev)
+    
+
+    p_reverse.innerHTML += rev
 
 
-split(palindrom)
+    if (res == rev){
+        console.log("Palindrom")
+    } else {
+        console.log("INTE")
+    }
+    
+
+}
