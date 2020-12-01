aa2a1ad5f3634fe7317b3d39e909774c04c4c9d0
Mariam656 <70264753+Mariam656@users.noreply.github.com>
2020-12-01 09:00:18 +0100

    v2

--- Programmering 1/palindrom/palindrom.js
+++ Programmering 1/palindrom/palindrom.js
@@ -15,7 +15,7 @@ knapp.addEventListener("click", Palindrom)
     let string = document.querySelector("#input").value
     let res = string.split("");
     
-    console.log(res)//skriv ut i konsol
+    console.log(res)//skriv ut i kons
    
     p_rak.innerHTML = string
     let rev = string.split("").reverse()
